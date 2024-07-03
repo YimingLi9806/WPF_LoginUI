f9706763d74297a3b68d310ac718c34026bb1554
YimingLi9806 <18126479711.li@gmail.com>
2024-07-02 23:01:42 -0400

    git desktop 2nd test

--- WPF_LoginUI/MainWindow.xaml.cs
+++ WPF_LoginUI/MainWindow.xaml.cs
@@ -52,7 +52,7 @@ namespace WPF_LoginUI
             else
             {
                 MessageBox.Show("no");
-                loginViewModel.LoginM.PassWord = "No";
+                loginViewModel.LoginM.PassWord = "NO";
                 loginViewModel.LoginM.UserName = "NO";
                 loginViewModel.LoginM = loginViewModel.LoginM;
                
